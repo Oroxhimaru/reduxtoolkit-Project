d702fde51b37f2b7934e89cc7d0601cecb6e4bd6
Oroxhimaru <hm51562@gmail.com>
2024-01-10 12:49:14 +0500

    css

--- reduxtoolkit-project/src/App.jsx
+++ reduxtoolkit-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import AddTodo from './components/AddTodo'
 import Todos from './components/Todos'
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
